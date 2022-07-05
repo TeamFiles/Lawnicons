f1c7a7a54ff173d4712acaa0a9a7c0cdb27d7e4e
Rufus Immanuel Raj P <68699122+rufus582@users.noreply.github.com>
2022-07-05 14:06:03 +0530

    UI: Bring back status bar

--- app/src/main/java/app/lawnchair/lawnicons/MainActivity.java
+++ app/src/main/java/app/lawnchair/lawnicons/MainActivity.java
@@ -25,10 +25,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
         setContentView(R.layout.activity_main);
 
         bottomNav = findViewById(R.id.bottomNav);
